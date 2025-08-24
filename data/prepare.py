148209f6c8605618c471018d45c84c1f39991644
mdaquin <mathieu.daquin.w+gh@gmail.com>
2025-08-24 15:15:36 +0200

    remove duplicates in data preparetion

--- data/prepare.py
+++ data/prepare.py
@@ -7,9 +7,14 @@ df['nbmuseum'] = df['nbmuseum'].astype(int)
 df['inmuseum'] = (df.nbmuseum > 0).astype(int)
 
 PE = df.loc[df['inmuseum'] == 1, ['desc', 'inmuseum']]
-PN = df.loc[df['inmuseum'] == 0, ['desc', 'inmuseum']].sample(int(len(PE)*IMB_FACT), random_state=42)
+PE = PE.drop_duplicates()
+PN = df.loc[df['inmuseum'] == 0, ['desc', 'inmuseum']]
+PN = PN.drop_duplicates()
+PN = PN.sample(int(len(PE)*IMB_FACT), random_state=42)
 df = pd.concat([PE, PN])
 df = df.sample(frac=1, random_state=42)
+
+print(df.duplicated().sum(), "duplicated entries")
 
 train = df.sample(frac=0.8, random_state=42)
 test = df.drop(train.index)
